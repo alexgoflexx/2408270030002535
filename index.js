4744026ee594cc9d1bba8b1aadfadb00946d2220
alexgoflexx <1053882678@qq.com>
2024-08-30 02:18:19 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 8081;
+
+// 提供静态文件
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 处理根请求，返回 HTML 文件
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});

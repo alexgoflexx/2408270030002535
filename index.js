e2807bc6f28ad4707d0de1f438c2bbaf0251f88f
alexgoflexx <1053882678@qq.com>
2024-08-30 13:52:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 8081;
+const port = 8080;
 
 // 提供静态文件
 app.use(express.static(path.join(__dirname, 'public')));

390e551d31bb965c94655a9e21c056c42b5f39ee
alexgoflexx <1053882678@qq.com>
2024-09-04 15:54:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 
 // 提供静态文件
 app.use(express.static(path.join(__dirname, 'public')));
